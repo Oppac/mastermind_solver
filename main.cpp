0041db285112938847712ca1e0e944f3c7086732
Oppac <oppac26@gmail.com>
2018-12-04 00:04:50 +0100

    vector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,17 +12,21 @@ int main()
   int nb_spots = 4;
   int nb_colors = 10;
 
-  string colors[nb_colors] = {"Red", "Orange", "Yellow", "Green", "Blue",
-                             "Indigo", "Violet", "Gray", "Black", "Pink"};
-  string secret[nb_spots];
+  string colors[nb_colors] = {"Red", "Orange", "Yellow",
+   "Green", "Blue", "Indigo", "Violet", "Gray", "Black", "Pink"};
+  vector<string> secret;
   string new_color;
   int i = 0;
+  bool in_secret;
 
   while(i < nb_spots)
   {
-     new_color = colors[rand() % 3];
-     secret[i] = new_color;
-     i++;
+     new_color = colors[rand() % 10];
+     in_secret = std::find(secret.begin(), secret.end(), new_color) != secret.end();
+     if (!in_secret){
+      secret.push_back(new_color);
+      i++;
+     }
   }
 
   for (auto x: secret) { cout << x << "\n"; }
