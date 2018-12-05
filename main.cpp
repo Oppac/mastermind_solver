cb8f09e67787b63ae6e6d3c6e87772bba64d98e9
Oppac <oppac26@gmail.com>
2018-12-05 05:46:06 +0100

    perfect and color only

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 void print_colors(int* secret, int nb_spots, string* colors_names);
-void new_solution(int* solution, int nb_spots, int nb_colors);
-bool check_solution(int* secret, int* guess, int nb_spots);
+void new_secret(int* solution, int nb_spots, int nb_colors);
+bool check_solution(int* secret, int* guess, int *perfect, int *colors_only, int nb_spots);
 
 bool in_array(int* arr, int arr_size, int nb);
 void reinitialize_arr(int* arr, int arr_size);
@@ -14,83 +14,90 @@ int main()
 {
   srand(time(NULL));
   int nb_spots = 4;
-  int nb_colors = 4;
+  int nb_colors = 5;
 
-  string colors_names[] = {"Red", "Orange", "Yellow",
+  string colors_names[] = {"None", "Red", "Orange", "Yellow",
   "Green", "Blue", "Indigo", "Violet", "Gray", "Black", "Pink"};
 
   int secret[nb_spots];
-  new_solution(secret, nb_spots, nb_colors);
-  cout << "\n";
+  new_secret(secret, nb_spots, nb_colors);
+  int perfect = 0;
+  int colors_only = 0;
 
   int guess[nb_spots];
   do {
-    new_solution(guess, nb_spots, nb_colors);
+    cout << "Perfect: " << perfect << " - Color Only: " << colors_only << "\n";
+    cout << "\n";
+    new_secret(guess, nb_spots, nb_colors);
     print_colors(guess, nb_spots, colors_names);
-  } while(check_solution(secret, guess, nb_spots) == 0);
+  } while(check_solution(secret, guess, &perfect, &colors_only, nb_spots) == 0);
 
   cout << "\nSecret: " << "\n";
   print_colors(secret, nb_spots, colors_names);
   cout << "Solution: " << "\n";
   print_colors(guess, nb_spots, colors_names);
 
-
   return 0;
 }
 
-
-void new_solution(int* solution, int nb_spots, int nb_colors)
+//Give a new random system
+void new_secret(int* secret, int nb_spots, int nb_colors)
 {
   int i = 0, new_color;
-  reinitialize_arr(solution, nb_spots);
+  reinitialize_arr(secret, nb_spots);
   while(i < nb_spots)
   {
-    new_color = rand() % nb_colors;
-    if (in_array(solution, nb_spots, new_color) == 0)
+    new_color = 1 + rand() % nb_colors;
+    if (in_array(secret, nb_spots, new_color) == 0)
     {
-      solution[i] = new_color;
+      secret[i] = new_color;
       i++;
     }
   }
 }
 
-bool check_solution(int* secret, int* guess, int nb_spots)
+//Check if the secret and the guess match
+bool check_solution(int* secret, int* guess, int *perfect, int *colors_only, int nb_spots)
 {
+  int b = 1;
+  *perfect = 0;
+  *colors_only = 0;
   for (int i = 0; i < nb_spots; ++i)
   {
-    if (secret[i] != guess[i])
+    if (secret[i] == guess[i])
+      (*perfect)++;
+    else
     {
-      return 0;
+      if (in_array(secret, nb_spots, guess[i]))
+        (*colors_only)++;
+      b = 0;
     }
   }
-  return 1;
+  return b;
 }
 
+//Print the array, replacing the numbers by their respective name
 void print_colors(int* secret, int nb_spots, string* colors_names)
 {
   for (int i = 0; i < nb_spots; ++i)
-  {
     cout << " " << colors_names[secret[i]] << " ";
-  }
   cout << "\n";
 }
 
+//Check if the color is already in the array
 bool in_array(int* arr, int arr_size, int nb)
 {
   for (int i = 0; i < arr_size; ++i)
   {
     if (arr[i] == nb)
-    {
       return 1;
-    }
   }
   return 0;
 }
 
+//Empty the array of its previous colors
 void reinitialize_arr(int* arr, int arr_size)
 {
   for (int i = 0; i < arr_size; ++i)
-  {
-    arr[i] = -1;
-  }
+    arr[i] = 0;
 }
