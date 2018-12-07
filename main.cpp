4418c44a4dfa0ac10e2fc70a86e6703cc404640a
Oppac <oppac26@gmail.com>
2018-12-07 20:17:44 +0100

    working mastermind solver

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ public:
   int id;
   int nb_spots;
   int nb_colors;
+  std::vector<int> secret;
   std::vector<std::vector<int>> possible_guesses;
 
   Solver(int id, int nb_spots, int nb_colors)
@@ -23,19 +24,27 @@ public:
     this->possible_guesses = solver_possible_guess();
   }
 
-  bool check_solution(vector<int> secret, vector<int> guess, int *perfect, int *colors_only);
+  Solver(int nb_spots, int nb_colors)
+  {
+    this->nb_spots = nb_spots;
+    this->nb_colors = nb_colors;
+    this->secret = new_secret(nb_spots, nb_colors);
+  }
 
   //Utility method
   void print_possible_guesses()
   {
-    for (int i=0; i < possible_guesses.size(); ++i)
+    if (!(possible_guesses.empty()))
     {
-      for (int j=0; j < nb_spots; ++j)
+      for (int i=0; i < possible_guesses.size(); ++i)
       {
-        std::cout << possible_guesses[i][j] << " ";
+        for (int j=0; j < nb_spots; ++j)
+          std::cout << possible_guesses[i][j] << " ";
+        std::cout << "\n";
       }
-      std::cout << "\n";
     }
+    else
+      std::cout << "No possible guess left\n";
   }
 
   int factorial(int n)
@@ -52,7 +61,7 @@ public:
     std::vector<std::vector<int>> all_guess;
     std::vector<int> possible_guess;
     std::vector<int> permu(nb_colors);
-    std::iota(permu.begin(), permu.end(), 1);
+    std::iota(permu.begin(), permu.end(), 0);
     int repeat = factorial(nb_colors-nb_spots);
     do
     {
@@ -79,9 +88,7 @@ public:
       if (all_guess[i][0] == id)
       {
         for(int j=0; j < nb_spots; ++j)
-        {
           guess.push_back(all_guess[i][j]);
-        }
         solver_guess.push_back(guess);
         guess.clear();
       }
@@ -92,32 +99,52 @@ public:
   std::vector<int> give_next_guess()
   {
     std::vector<int> guess;
-    for(int i=0; i < nb_spots; ++i)
-      guess.push_back(possible_guesses[0][i]);
+    if (!(possible_guesses.empty()))
+    {
+      for(int i=0; i < nb_spots; ++i)
+        guess.push_back(possible_guesses[0][i]);
       possible_guesses.erase(std::remove(possible_guesses.begin(),
                              possible_guesses.end(), guess), possible_guesses.end());
+    }
     return guess;
   }
 
-  void update_possible_guesses(std::vector<int> guess, int perfect, int colors_only)
+  void update_possible_guesses(std::vector<int> old_guess, int perfect, int colors_only)
   {
-
+    int tmp_perfect = perfect;
+    int tmp_colors_only = colors_only;
+    std::vector<int> new_guess;
+    for (int i=0; i < possible_guesses.size(); ++i)
+    {
+      for (int j=0; j < nb_spots; ++j)
+        new_guess.push_back(possible_guesses[i][j]);
+      check_solution(old_guess, new_guess, &tmp_perfect, &tmp_colors_only);
+      if ((tmp_perfect != perfect) || (tmp_colors_only != colors_only))
+        possible_guesses.erase(std::remove(possible_guesses.begin(),
+                               possible_guesses.end(), new_guess), possible_guesses.end());
+      new_guess.clear();
+    }
   }
 
-};
 
-class Master
-{
-public:
-  int nb_spots;
-  int nb_colors;
-  std::vector<int> secret;
-
-  Master(int nb_spots, int nb_colors)
+  //Check if the guess is correct or not
+  bool check_solution(std::vector<int> secret, std::vector<int> guess, int *perfect, int *colors_only)
   {
-      this->nb_spots = nb_spots;
-      this->nb_colors = nb_colors;
-      this->secret = new_secret(nb_spots, nb_colors);
+    int b = 1;
+    *perfect = 0;
+    *colors_only = 0;
+    for (int i = 0; i < secret.size(); ++i)
+    {
+      if (secret[i] == guess[i])
+        (*perfect)++;
+      else
+      {
+        if (std::find(secret.begin(), secret.end(), guess[i]) != secret.end())
+          (*colors_only)++;
+        b = 0;
+      }
+    }
+    return b;
   }
 
   //Give a new random system
@@ -141,30 +168,9 @@ public:
 
 using namespace std;
 
-bool check_solution(vector<int> secret, vector<int> guess, int *perfect, int *colors_only);
 void print_colors(vector<int> secret, int nb_spots, vector<string>  colors_names);
 void print_colors(vector<int> secret, int nb_spots, vector<string>  colors_names,
                   int perfect, int colors_only);
-
-//Check if the guess is correct or not
-bool check_solution(vector<int> secret, vector<int> guess, int *perfect, int *colors_only)
-{
-  int b = 1;
-  *perfect = 0;
-  *colors_only = 0;
-  for (int i = 0; i < secret.size(); ++i)
-  {
-    if (secret[i] == guess[i])
-      (*perfect)++;
-    else
-    {
-      if (find(secret.begin(), secret.end(), guess[i]) != secret.end())
-        (*colors_only)++;
-      b = 0;
-    }
-  }
-  return b;
-}
 
 //Print the colors
 void print_colors(vector<int> secret, int nb_spots, vector<string> colors_names)
@@ -196,9 +202,10 @@ int main(int argc, char* argv[])
   vector<string> colors_names = {"Red", "Orange", "Yellow",
   "Green", "Blue", "Indigo", "Violet", "Gray", "Black", "Pink"};
 
-  Master master_node(nb_spots, nb_colors);
-  Solver solver_1(1, nb_spots, nb_colors);
-  //s.print_possible_guesses();
+  Solver master_node(nb_spots, nb_colors);
+  vector<Solver*> solvers;
+  for (int i=0; i < nb_colors; i++)
+    solvers.push_back(new Solver(i, nb_spots, nb_colors));
 
   //int rank, nb_instances;
   //MPI_Status status;
@@ -207,16 +214,27 @@ int main(int argc, char* argv[])
   //MPI_Comm_size(MPI_COMM_WORLD, &nb_instances);
 
   bool found = 0;
-  int i = 0;
-  do {
-    i++;
-    //MPI_Status status;
-    vector<int> guess = solver_1.give_next_guess();
-    found = check_solution(master_node.secret, guess, &perfect, &colors_only);
-    //solver_1.update_possible_guesses(guess, perfect, colors_only);
-    print_colors(guess, nb_spots, colors_names, perfect, colors_only);
-  } while (i < 10);
-  //} while (!found);
+  for (auto const &s: solvers)
+  {
+    while (!(s->possible_guesses.empty()))
+    {
+      vector<int> guess = s->give_next_guess();
+      found = master_node.check_solution(master_node.secret, guess, &perfect, &colors_only);
+      if (found)
+        break;
+      s->update_possible_guesses(guess, perfect, colors_only);
+      //cout << "\n";
+      //s->print_possible_guesses();
+      print_colors(guess, nb_spots, colors_names, perfect, colors_only);
+    }
+    if (found)
+    {
+      cout << "Solution found by solver " << s->id << "\n";
+      break;
+    }
+
+  }
+
 
   cout << "\nSolution: ";
   print_colors(master_node.secret, nb_spots, colors_names);
